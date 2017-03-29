60e331ce5409ac9846574c150bc9ba9edbb2b535
James Hill <j.g.hill@zoho.com>
2017-03-29 19:48:48 +0100

    1st attempt at implementing fibonacci sequence as pattern matched function.

--- part1.erl
+++ part1.erl
@@ -1,5 +1,7 @@
 -module(part1).
 -export([fib/1]).
 
-fib(N) ->
-    
+% A function for calculating the Fibonacci Sequence.
+fib(N)          when N > 0 -> fib(N, 1, 0, 1).
+fib(N, A, B, C) when N = A -> C.
+fib(N, A, B, C) when N < A -> fib(N, A + 1, C, B + C).
