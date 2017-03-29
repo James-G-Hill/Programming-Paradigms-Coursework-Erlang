4f0cd18d08f64f89a3e4b5a7228c8d0996826cc8
James Hill <j.g.hill@zoho.com>
2017-03-29 21:19:42 +0100

    Completed the Fibonacci Sequence function.

--- part1.erl
+++ part1.erl
@@ -2,6 +2,10 @@
 -export([fib/1]).
 
 % A function for calculating the Fibonacci Sequence.
-fib(N)          when N > 0 -> fib(N, 1, 0, 1).
-fib(N, A, B, C) when N = A -> C.
-fib(N, A, B, C) when N < A -> fib(N, A + 1, C, B + C).
+fib(N)           when N < 0  -> "Error: sequence number must be 0 or greater.";
+fib(N)           when N == 0 -> 0;
+fib(N)           when N > 0  -> fib(N, 1, 0, 1).
+
+% A helper function for calculating the Fibonacci Sequence.
+fib(N, A, _B, C) when N =< A -> C;
+fib(N, A, B, C)  when N > A  -> fib(N, A + 1, C, B + C).
