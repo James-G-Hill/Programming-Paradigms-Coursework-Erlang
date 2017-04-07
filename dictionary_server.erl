06a7b5cfbd5f34eb9531c1f55c1a92a25c2e2dd3
unknown <>
2017-04-07 22:14:32 +0100

    Added 'insert' function and record creation.

--- dictionary_server.erl
+++ dictionary_server.erl
@@ -1,5 +1,10 @@
 -module(dictionary_server).
--export([start/0, stop/0]).
+-export([start/0, stop/0, insert/2]).
+
+%% The record structure.
+
+-record(item, {key, value}).
+
 
 %% Functions for creating and initializing the server.
 
@@ -12,7 +17,8 @@ init() ->
 
 %% Client functions.
 
-stop() -> call(stop).
+stop()   -> call(stop).
+insert(K, V) -> call({insert, K, V}).
 
 
 %% Message passing.
@@ -30,8 +36,9 @@ end.
 loop() ->
     receive
 	{request, Pid, stop} ->
-	    reply(Pid, ok)
+	    reply(Pid, stopped), loop();
+	{request, Pid, {insert, K, V}} ->
+	    #item{key=K, value=V}, reply(Pid, inserted), loop()
 end.
 
-reply(Pid, Message) ->
-    Pid ! {reply, Message}.
+reply(Pid, Message) -> Pid ! {reply, Message}.
